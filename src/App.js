8a50f3e94ee97d009ca1f2f8be8396f73e00c6d5
wprkftlgus <wprkftlgus@naver.com>
2025-07-14 14:47:14 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,5 +1,100 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef ,useState, useEffect, use } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import './App.css';
+import { EffectComposer, Bloom } from '@react-three/postprocessing';
+
+function Scene() {
+  const groupRef = useRef();
+  const sunRef = useRef();
+  const earthRef = useRef();
+  const moonRef = useRef();
+  const marsRef = useRef();
+  const [mouse, setMouse] = useState({x: 0, y: 0});
+
+  const earth = useGLTF("/earth.glb");
+  const moon = useGLTF("/moon.glb");
+  const mars = useGLTF("/mars.glb");
+  const sun = useGLTF("hot_sun.glb")
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const x = (e.clientX / window.innerWidth) * 2 - 1;
+      const y = -(e.clientY / window.innerHeight) * 2 + 1;
+      setMouse({ x, y });
+    };
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.rotation.y = mouse.x * 1;
+      groupRef.current.rotation.x = mouse.y * 1;
+    }
+     if (earthRef.current) {
+      earthRef.current.rotation.y += 0.003;  
+    }
+    if (moonRef.current) {
+      moonRef.current.rotation.y += 0.01;  
+    }
+    if (marsRef.current) {
+      marsRef.current.rotation.y += 0.007;  
+    }
+  })
+return(
+  <group ref={groupRef}>
+    <Orbit speed={0.1} radius={6}>
+    <primitive ref={marsRef} object={mars.scene} scale={1.8} position={[12, 0, 1]} />
+    </Orbit>
+    <Orbit speed={0.2} radius={3}>
+    <primitive ref={earthRef} object={earth.scene} scale={0.8}  position={[4, 0, 1]}/>
+    </Orbit>
+    <Orbit speed={0} radius={0}>
+      <primitive ref={sunRef} object={sun.scene} scale={0.7} position={[0, 0, 0]}/>
+    </Orbit>
+    <MoonOrbit speed={0.5} radius={1}>
+      <primitive ref={moonRef} object={moon.scene} scale={0.3} position={[7, 0, 1]}/>
+    </MoonOrbit>
+    
+    
+  </group>
+);
+};
+
+function MoonOrbit({speed , radius, children}) {
+  const groupRef = useRef();
+
+  useFrame(({ clock }) => {
+    const elapsed = clock.getElapsedTime();
+    groupRef.current.rotation.y = elapsed * speed;
+  });
+
+  return (
+    <group ref={groupRef}>
+      <group position={[radius, 0, 0]}>
+        {children}
+      </group>
+    </group>
+  );
+}
+
+function Orbit({ speed, radius, children }) {
+  const groupRef = useRef();
+
+  useFrame(({ clock }) => {
+    const elapsed = clock.getElapsedTime();
+    groupRef.current.rotation.y = elapsed * speed;
+  });
+
+  return (
+    <group ref={groupRef}>
+      <group position={[radius, 0, 0]}>
+        {children}
+      </group>
+    </group>
+  );
+}
 
 function App() {
   const [position, setPosition] = useState({x:0, y:0});
@@ -10,6 +105,7 @@ function App() {
     window.addEventListener('mousemove', moveHandler);
     return() => window.removeEventListener('mousemove', moveHandler);
   },[]);
+
 
   return (
     <div className='App'>
@@ -22,29 +118,16 @@ function App() {
       <div className='Top-tag3'>Contact</div>
       
       </div>
-
-    <div className='Title'>All-in-one Developer</div>
-    <div> Hello! I'm a developer from South Korea, with a degree in Naval Architecture and Ocean Engineering from the University of Ulsan.
-I'm skilled in both frontend and backend development, and I enjoy creating complete, full-stack web applications that look great and work flawlessly.</div>
-  <div className='project'>
-  <div></div>
-  </div>
-   </div>
-  <div className='About'>
-   <div className='Body-title1'>About Me</div>
-   <ul>
-   <li>Naval-Englineer</li>
-   <li>Korea Army 1Year-Half</li> 
-   <li>Airbnb Business for 6 months</li> 
-   <li>Worked as manager in company</li>  
-   </ul>  
-  </div>
-  <div className='Project'>
-   <div className='Body-title2'>Project</div> 
-  </div>
-
-
-
+      <Canvas camera={{ position: [10, 0, 12] }} >
+        <Scene />
+        <pointLight position={[0, 0, 0]} intensity={400} color="#edebe1ff" />
+        <EffectComposer>
+         <Bloom luminanceThreshold={10} luminanceSmoothing={1} height={100} />
+         </EffectComposer>
+        <directionalLight position={[-2, 2, 2]} intensity={1} />
+        <ambientLight position={[2, 2, 2]} intensity={1} />
+      </Canvas>
+      </div>
   </div>
   );
 }
